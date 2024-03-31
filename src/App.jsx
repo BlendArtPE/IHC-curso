ee90460c31a8a956f12657cdbf537003015967b7
Daniel Sevillano <jesus.cs.7.2010@gmail.com>
2024-03-31 14:32:42 -0500

    Imagen y Separación

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import main from "./mock/main.json"
+import index from "./mock/main.json"
 import Nav from "./components/Nav"
 import Main from "./components/Main"
+import Categories from "./components/Categories"
 function App() {
 
   return (
     <>
         <div className="relative">
           <div className="fixed top-0 left-0 right-0 z-10">
-            <Nav data={main.sections.nav} />
+            <Nav data={index.sections.nav} />
           </div>
         </div>
-        <Main data={main.sections.main} />
+        <Main data={index.sections.main} />
+        <div className="h-4"/>
+        <Categories data={index.sections.categories} />
     </>
   )
 }

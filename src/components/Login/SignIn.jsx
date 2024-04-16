57a1d592f318c1969a129e826907c340fe1100b8
Daniel Sevillano <jesus.cs.7.2010@gmail.com>
2024-04-16 16:22:00 -0500

    Sonidos en Login y Register

--- src/components/Login/SignIn.jsx
+++ src/components/Login/SignIn.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import email from "../../../public/audios/email.mp3"
+import password from "../../../public/audios/password.mp3"
+import buttonLogin from "../../../public/audios/buttonLogin.mp3"
+import loginGoogle from "../../../public/audios/loginGoogle.mp3"
+import loginFacebook from "../../../public/audios/loginFacebook.mp3"
+import optionRegister from "../../../public/audios/optionRegister.mp3"
+import rememberUser from "../../../public/audios/rememberUser.mp3"
+
 
 const SignIn = () => {
     const [safe, setSafe] = useState(true)
+
+    const playSound = (sound) => {
+        const audio = new Audio(sound);
+        audio.play();
+    };
     
     return (
       <section className="xl:mt-20 mt-10 h-screen  bg-[#F9F9F9] flex">
@@ -15,25 +28,25 @@ const SignIn = () => {
                 <p className="text-sm  mb-4">Ingresa tu correo y contraseña para acceder a tu cuenta</p>
 
                 <h3 className="text-xl mb-2">Correo electrónico</h3>
-                <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu correo" id="" />
+                <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu correo" id="" onFocus={() => {playSound(email)}} />
 
                 <h3 className="text-xl mb-2">Contraseña</h3>
                 <div className="relative">
-                    <input type={safe ? "password" : "text"} name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu contraseña" id="" />
+                    <input type={safe ? "password" : "text"} name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu contraseña" onFocus={() => {playSound(password)}} id="" />
                     <i onClick={() => setSafe(!safe)} className={`absolute inset-y-1/4 right-4 fa-regular ${safe ? "fa-eye-slash" : "fa-eye"}`}></i>
                 </div>
                 
                 <div className="flex justify-between items-center text-sm">
                     <div className="flex gap-1">
-                        <input type="checkbox" name="" id="" />
+                        <input type="checkbox" name="" id="" onFocus={() => {playSound(rememberUser)}} />
                         <p>Recuérdame</p>
                     </div>
                     <a className="text-sky-500">¿Olvidaste tu contraseña?</a>
                 </div>
 
-                <button type="submit" className="w-full py-2 my-2 bg-black rounded-md text-white">Ingresar</button>
+                <button onFocus={() => {playSound(buttonLogin)}} type="submit" className="w-full py-2 my-2 bg-black rounded-md text-white">Ingresar</button>
 
-                <p className="text-center">¿No tienes una cuenta? <Link className="text-sky-500 hover:underline underline-offset-4" to="/signup">Regístrate</Link></p>
+                <p className="text-center">¿No tienes una cuenta? <Link className="text-sky-500 hover:underline underline-offset-4" to="/signup"onFocus={() => {playSound(optionRegister)}} >Regístrate</Link></p>
 
                 <div className="flex items-center my-4">
                     <hr className="w-full"/>
@@ -42,12 +55,12 @@ const SignIn = () => {
                 </div>
 
                 <div className="flex gap-2">
-                    <button className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
+                    <button onFocus={() => {playSound(loginGoogle)}} className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
                         <img src="./google.svg" className="h-8" alt="" />
                         <p>Google</p>
                     </button>
 
-                    <button className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
+                    <button onFocus={() => {playSound(loginFacebook)}} className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
                         <img src="./fb.svg" className="h-8" alt="" />
                         <p>Facebook</p>
                     </button>
--- src/components/Login/SignUp.jsx
+++ src/components/Login/SignUp.jsx
@@ -1,8 +1,23 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import enterNames from "../../../public/audios/enterNames.mp3"
+import email from "../../../public/audios/email.mp3"
+import enterLastNames from "../../../public/audios/enterLastNames.mp3"
+import enterPasswords from "../../../public/audios/enterPasswords.mp3"
+import enterPhone from "../../../public/audios/enterPhone.mp3"
+import registerUser from "../../../public/audios/registerUser.mp3"
+import haveUser from "../../../public/audios/haveUser.mp3"
+import loginGoogle from "../../../public/audios/loginGoogle.mp3"
+import loginFacebook from "../../../public/audios/loginFacebook.mp3"
 
 const SignUp = () => {
     const [safe, setSafe] = useState(true)
+
+    const playSound = (sound) => {
+        const audio = new Audio(sound);
+        audio.play();
+    };
+
 
     return (
       <section className="xl:mt-20 lg:mt-20 md:mt-10 mt-20 xl:h-screen lg:h-auto md:h-screen bg-[#F9F9F9] flex">
@@ -16,26 +31,26 @@ const SignUp = () => {
                 <div className="flex gap-3">
                     <div className="w-full">
                         <h3 className="text-xl mb-2">Nombres</h3>
-                        <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresas tus nombres" id="" />
+                        <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresas tus nombres" id="" onFocus={() => {playSound(enterNames)}} />
                     </div>
                     <div className="w-full">
                         <h3 className="text-xl mb-2">Apellidos</h3>
-                        <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tus apellidos" id="" />
+                        <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tus apellidos" id="" onFocus={() => {playSound(enterLastNames)}} />
                     </div>
                 </div>
 
+                <h3 className="text-xl mb-2">Correo electrónico</h3>
+                <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu correo electrónico" id="" onFocus={() => {playSound(email)}} />
 
                 <h3 className="text-xl mb-2">Contraseña</h3>
                 <div className="relative">
-                    <input type={safe ? "password" : "text"} name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu contraseña" id="" />
+                    <input type={safe ? "password" : "text"} name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu contraseña" id="" onFocus={() => {playSound(enterPasswords)}} />
                     <i onClick={() => setSafe(!safe)} className={`absolute inset-y-1/4 right-4 fa-regular ${safe ? "fa-eye-slash" : "fa-eye"}`}></i>
                 </div>
 
-                <h3 className="text-xl mb-2">Contraseña</h3>
-                <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu contraseña" id="" />
 
                 <h3 className="text-xl mb-2">Teléfono</h3>
-                <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu teléfono" id="" />
+                <input type="text" name="" className="p-2 mb-4 w-full text-sm border-2 rounded-md" placeholder="Ingresa tu teléfono" id="" onFocus={() => {playSound(enterPhone)}} />
                 
                 {/* <div className="flex justify-between items-center text-sm">
                     <div className="flex gap-1">
@@ -45,9 +60,9 @@ const SignUp = () => {
                     <a className="text-sky-500">¿Olvidaste tu contraseña?</a>
                 </div> */}
 
-                <Link to="/signin" className="w-full py-2 text-center my-2 bg-black rounded-md text-white">Registrar</Link>
+                <Link to="/signin" onFocus={() => {playSound(registerUser)}} className="w-full py-2 text-center my-2 bg-black rounded-md text-white">Registrar</Link>
 
-                <p className="text-center">¿Tienes una cuenta? <Link className="text-sky-500 hover:underline underline-offset-4" to="/signin">Ingresa</Link></p>
+                <p className="text-center">¿Tienes una cuenta? <Link className="text-sky-500 hover:underline underline-offset-4" to="/signin" onFocus={() => {playSound(haveUser)}} >Ingresa</Link></p>
 
                 <div className="flex items-center my-4">
                     <hr className="w-full"/>
@@ -56,12 +71,12 @@ const SignUp = () => {
                 </div>
 
                 <div className="flex gap-2">
-                    <button className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
+                    <button onFocus={() => {playSound(loginGoogle)}} className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
                         <img src="./google.svg" className="h-8" alt="" />
                         <p>Google</p>
                     </button>
 
-                    <button className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
+                    <button onFocus={() => {playSound(loginFacebook)}} className="flex gap-2 py-1 border-2 rounded-md items-center w-full justify-center">
                         <img src="./fb.svg" className="h-8" alt="" />
                         <p>Facebook</p>
                     </button>

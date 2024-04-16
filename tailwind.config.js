f23320275b12ee23add25344b39130f9fcbf27c8
DAVID SANCHEZ <72284986+David-Sanchez-Sangay@users.noreply.github.com>
2024-04-15 23:54:42 -0500

    Change of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,16 @@ export default {
     extend: {
       colors: {
         color: {
-          '1': '#424242',
+          '1': '#002337',//#424242
           '2': '#FFFFFF',
           '3': '#F0F0F0',
-          '4': '#343434',
+          '4': '#00425D',//'#343434'
           '5': '#1496EA',
           '6': '#147FC5',
           '7': '#585858',
           '8': '#FAF7F7',
           '9': '#9FA3A8',
+          '10':'#006C8B',
           'sombra': '#525252',
           // Puedes agregar más colores según tus necesidades
         },
